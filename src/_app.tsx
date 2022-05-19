bcc4b0c119322fe089ffbdb4e4c3e5e29c10ef48
Chris Fryer <chris.fryer@carlotz.com>
2022-05-19 10:44:45 -0600

    initial commit of remote template

---
+++ src/_app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<div>Remote: {REPLACE_NAME} on port {REPLACE_PORT}</div>
+	);
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+	entry: "./src/index.ts",
+	mode: "development",
+	devServer: {
+		port: 3003,
+		open: false,
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js"],
+		alias: {
+			'@shared': path.resolve(__dirname, '../../shared')
+		}
+	},
+	output: {
+		publicPath: 'http://localhost:{REPLACE_PORT}/',
+	},
+	module: {
+		rules: [ 
+			{ test: /\.(js|jsx|tsx|ts)$/, loader: "ts-loader", exclude: /node_modules/ },
+			{ test: /\.(css|scss|sass)$/, use: ["style-loader", "css-loader"]}
+		],
+	},
+	plugins: [
+		new ModuleFederationPlugin({
+			name: "remote_{REPLACE_NAME}",
+			filename: 'remote.js',
+			exposes: {
+				'./Application': './src/_app',
+      			},
+			shared: {
+				...deps,
+				'react': {
+					singleton: true,
+					requiredVersion: deps.react,
+				},
+				'react-dom': {
+					singleton: true,
+					requiredVersion: deps['react-dom'],
+				},
+			},
+		}),
+		new HtmlWebpackPlugin({
+			template: "./public/index.html",
+		}),
+	],
+};
